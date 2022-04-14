4500de042bf6788481b5bb891eb56627cb3eab94
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-04-15 01:55:13 +0200

    implementing Dragging functionality

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ themeBtn.addEventListener("click", () => {
 
 const createElement = (text) => {
   html = `
-  <li class="list-item">
+  <li class="list-item" draggable="true">
               <button class="btn-check"></button>
               <span class="text">${text}</span>
               <button class="btn-delete">
@@ -39,6 +39,7 @@ form.addEventListener("submit", (e) => {
     checkComplete();
     deleteElement();
     counterUpdate(1);
+    draggableElement();
   }
 });
 
@@ -123,4 +124,61 @@ function filterTodo(button) {
 
 function counterUpdate(num) {
   items.innerText = Number(items.innerText) + num;
+}
+
+// Draggable Function
+function draggableElement() {
+  const draggables = todoUI.querySelectorAll(".list-item");
+
+  // Drag start and drag end event
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+      draggable.classList.add("dragging");
+    });
+
+    draggable.addEventListener("dragend", () => {
+      draggable.classList.remove("dragging");
+    });
+  });
+  // this function helps to keep element draggable only in container div
+  todoUI.addEventListener("dragover", (e) => {
+    // By defaulet dragg in inside ne element is disabled, this enables it
+    e.preventDefault();
+    const afterEl = dragElement(todoUI, e.clientY);
+    // Only one element has a class dragging in ul list
+    const drag = document.querySelector(".dragging");
+    // if we are not above anything
+    if (afterEl == null) {
+      todoUI.appendChild(drag);
+    } else {
+      todoUI.insertBefore(drag, afterEl);
+    }
+  });
+}
+
+// This function determine in which position your drag element is after we start to drag
+// y it determines our mouse position
+function dragElement(container, y) {
+  const draggableEl = [...container.querySelectorAll("li:not(.dragging)")];
+  // this determines our mouse position after en element
+  return draggableEl.reduce(
+    // first element is value of closest element after mouse cursor
+    // second element is these draggable elements
+    (closest, child) => {
+      // this function is going to give us a rectangel for our box
+      const box = child.getBoundingClientRect();
+      // distance between center of the box and ouu actual cursor
+      const offset = y - box.top - box.height / 2;
+
+      // we are interesting for offset under 0 essentiall we're above the element taht we're hoverin over
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    // initial value of our reduce
+    // Negative infinity is that we need this offset value to be far away from our cursor
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
